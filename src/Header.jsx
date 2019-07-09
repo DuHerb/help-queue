16a035b557ea34566401547043d0bec5daa4ec82
Student <example@example.com>
2019-07-09 14:43:24 -0700

    add site links

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,8 +1,19 @@
 import React from 'react';
+import { Link }from 'react-router-dom';
+import Moment from 'react-moment';
 
 function Header() {
+  const date = new Date();
   return (
-    <h1>Help Queue</h1>
+    <>
+      <h1>Help Queue</h1>
+      <Link to="/">Home</Link><br/>
+      <Link to="/newticket">New Ticket</Link>
+      <div>
+        <p>Today is <Moment format="dddd">{date}</Moment></p>
+      </div>
+      
+    </>
   );
 }
 
